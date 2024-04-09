ffe7794d5868a51b3699a230ccb3f8c6214d5529
poofy25 <117314241+poofy25@users.noreply.github.com>
2024-04-09 17:30:26 +0300

    finished the fornt-end

--- src/app/emergency/page.js
+++ src/app/emergency/page.js
@@ -24,14 +24,14 @@ export default function Emergency () {
 
     return (
         <div className="w-full h-[100vh] bg-gray text-black">
-            <Navbar page='profile'/>
+            <Navbar page='none'/>
             <div className="w-full h-full justify-start flex flex-col items-center">
                 <h2 className="my-8 text-2xl font-bold">De ce urgență ai nevoie?</h2>
                 <div className="flex w-[450px] flex-wrap gap-8 gap-x-16 justify-center">
-                    <button onClick={()=>setSelected(1)} className={`${selected === 1 ? "bg-primaryTransparent" : ""} transition-all p-2 rounded-xl bg-transparent font-bold text-xl flex flex-col gap-2 justify-center items-center`}><div className="relative w-[175px] h-[125px]"><Image className="w-full h-full object-contain" src={AmbulantaImg} width='500' height='500'/></div>Ambulanță</button>
-                    <button onClick={()=>setSelected(2)} className={`${selected === 2 ? "bg-primaryTransparent" : ""} transition-all p-2 rounded-xl bg-transparent font-bold text-xl flex flex-col gap-2 justify-center items-center`}><div className="relative w-[175px] h-[125px]"><Image className="w-full h-full object-contain" src={PompieriImg} width='500' height='500'/></div>Pompieri</button>
-                    <button onClick={()=>setSelected(3)} className={`${selected === 3 ? "bg-primaryTransparent" : ""} transition-all p-2 rounded-xl bg-transparent font-bold text-xl flex flex-col gap-2 justify-center items-center`}><div className="relative w-[175px] h-[125px]"><Image className="w-full h-full object-contain" src={PolitieImg} width='500' height='500'/></div>Poliția</button>
-                    <button onClick={()=>setSelected(4)} className={`${selected === 4 ? "bg-primaryTransparent" : ""} transition-all p-2 rounded-xl bg-transparent font-bold text-xl flex flex-col gap-2 justify-center items-center`}><div className="relative w-[175px] h-[125px]"><Image className="w-full h-full object-contain" src={ServiceImg} width='500' height='500'/></div>Service 24/24</button>
+                    <button onClick={()=>setSelected('Ambulanta')} className={`${selected === 'Ambulanta' ? "bg-primaryTransparent" : ""} transition-all p-2 rounded-xl bg-transparent font-bold text-xl flex flex-col gap-2 justify-center items-center`}><div className="relative w-[175px] h-[125px]"><Image className="w-full h-full object-contain" src={AmbulantaImg} width='500' height='500'/></div>Ambulanță</button>
+                    <button onClick={()=>setSelected('Pompieri')} className={`${selected === 'Pompieri' ? "bg-primaryTransparent" : ""} transition-all p-2 rounded-xl bg-transparent font-bold text-xl flex flex-col gap-2 justify-center items-center`}><div className="relative w-[175px] h-[125px]"><Image className="w-full h-full object-contain" src={PompieriImg} width='500' height='500'/></div>Pompieri</button>
+                    <button onClick={()=>setSelected('Politie')} className={`${selected === 'Politie' ? "bg-primaryTransparent" : ""} transition-all p-2 rounded-xl bg-transparent font-bold text-xl flex flex-col gap-2 justify-center items-center`}><div className="relative w-[175px] h-[125px]"><Image className="w-full h-full object-contain" src={PolitieImg} width='500' height='500'/></div>Poliția</button>
+                    <button onClick={()=>setSelected('Service')} className={`${selected === 'Service' ? "bg-primaryTransparent" : ""} transition-all p-2 rounded-xl bg-transparent font-bold text-xl flex flex-col gap-2 justify-center items-center`}><div className="relative w-[175px] h-[125px]"><Image className="w-full h-full object-contain" src={ServiceImg} width='500' height='500'/></div>Service 24/24</button>
                 </div>
                 <button onClick={()=>{router.push('/map')}} disabled={selected === null ? true : false} className="disabled:opacity-50 mt-8 bg-primary text-white font-semibold w-[400px] py-3 rounded-lg">Continuă</button>
             </div>
